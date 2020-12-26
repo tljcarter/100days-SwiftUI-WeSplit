6c3f4c05d5ba2c4365a5730b2b85baaffb267b20
Tom L carter <tom@tljcarterhome.com>
2020-12-26 14:36:25 -0600

    Setup of WeSplit repo

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Tom Carter on 12/14/20.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
